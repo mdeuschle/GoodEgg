9a0994e932d4a2ce5c8d6e47122245e08cd069ed
Matthew Deuschle <mdeuschle@gmail.com>
2019-03-31 18:55:27 -0700

    remove comment line

---
+++ GoodEgg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodEgg
+//
+//  Created by Matt Deuschle on 3/31/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
